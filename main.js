112d042eac7e22ac85a467035295f4a2e4f5bb11
RavinakS <ravinasingh697@gmail.com>
2021-07-15 06:52:31 +0530

    created a a promise for signUp

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const readline = require('readline-sync');
-const user = readline.question('Login(L/l) or SignUp(S/s):- ')
-if(user==='l'){
-    console.log('please enter your email: ');
-}else if(user==='s'){
-    console.log('please enter your Name: ');
+
+const user = readline.question('Login(L/l) or SignUp(S/s):- ');
+
+function signUp(){
+    return new Promise ((resolve, reject) => {
+        const userName = readline.question("Type your userName:- ");
+        const password1 = readline.question("Enter password:- ");
+        const password2 = readline.question("Re-enter the password:- ");
+        resolve([userName, password1, password2]);
+    })
+}
+
+if(user==='s' || user === 'S'){
+    signUp().then((array) =>{
+        // console.log(array);
+        // console.log(typeof(array));
+        console.log(array[0]);
+        console.log(array[1]);
+        console.log(array[2]);
+    })
 }
