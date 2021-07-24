e583a26d26c916e29e5a96e85f1468dde6c33bae
RavinakS <ravinasingh697@gmail.com>
2021-07-24 23:36:58 +0530

    added password validation

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const readline = require('readline-sync');
+const fs = require('fs');
+const { resolve } = require('path');
 
 const user = readline.question('Login(L/l) or SignUp(S/s):- ');
 
@@ -11,13 +13,52 @@ const user = readline.question('Login(L/l) or SignUp(S/s):- ');
 //     })
 // }
 
+function isNumberInString(password){
+    return new Promise((resolve, reject)=>{
+        const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        var index = 0;
+        var checker = 0
+        for(index; index<password.length; index++){
+            try{
+                var paswrdNum = parseInt(password[index], 10);
+                if(numbers.includes(paswrdNum)){
+                    checker ++;
+                }
+            }catch{
+                continue;
+            }
+        }
+
+        if(checker > 0){
+            resolve("Valid");
+        }else{
+            reject("Unvalid");
+        }
+    })
+}
+
 function passwordValidation(password){
     return new Promise ((resolve, reject)=>{
         if(password.includes("@") || password.includes("#")){
-            
+            const isNumber = isNumberInString(password)
+            if(isNumber === "Valid"){
+                resolve("Valid");
+            }
         }else{
             console.log("Atleas password should contain one special character and one number.");
         }
+    })
+}
+
+function readJSONFile(fileName){
+    return new Promise((resolve, reject)=>{
+
+    })
+}
+
+function writeJsonFile(fileName, data){
+    return new Promise((resolve, reject)=>{
+
     })
 }
 
@@ -26,12 +67,7 @@ if(user ==='s' || user === 'S'){
     const password1 = readline.question("Enter password:- ");
     const password2 = readline.question("Re-enter the password:- ");
     if(password1 === password2){
-        console.log("Congrats You have signed Up.");
-        // signUp().then((array) =>{
-        //     console.log(array[0]);
-        //     console.log(array[1]);
-        //     console.log(array[2]);
-        // })
+        passwordValidation.then(())
     }else{
         console.log("Both Passwords are not same.");
     }
